befd5b522a21efc0c7065209c93ac2c3e237c0d9
sharkgamerofi-coder <sharkgamerofi@gmail.com>
2026-05-08 15:58:07 -0600

    Crear loader principal KeiloHub.lua

---
+++ KeiloHub.lua
@@ -0,0 +1,9 @@
+-- == Keilo Hub Loader ==
+-- Loader principal para Keilo Hub
+
+local scriptUrl = "https://raw.githubusercontent.com/sharkgamerofi-coder/Keilo-Hub/main/"
+
+print("[Keilo Hub] Cargando...")
+
+-- Aquí pondremos el menú o loader más adelante
+print("Keilo Hub listo. Esperando instrucciones del usuario.")
